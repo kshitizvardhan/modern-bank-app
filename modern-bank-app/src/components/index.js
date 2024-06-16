ba77d2695ce18a4e44ca1ba8407a13ec7de6a428
kshitizvardhan <kshitizvardhan8@gmail.com>
2024-06-17 00:42:20 +0530

    GetStarted Component, defined-style

---
+++ modern-bank-app/src/components/index.js
@@ -0,0 +1,31 @@
+// a trick/way to get all components in single line on app.jsx
+
+import Billing from "./Billing";
+import Business from "./Business";
+import Button from "./Button";
+import CardDeal from "./CardDeal";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import FeedBackCard from "./FeedBackCard";
+import Footer from "./Footer";
+import GetStarted from "./GetStarted";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import Stats from "./Stats";
+import Testimonials from "./Testimonials";
+
+export {
+    Billing,
+    Business,
+    Button,
+    CardDeal,
+    Clients,
+    CTA,
+    FeedBackCard,
+    Footer,
+    GetStarted,
+    Hero,
+    Navbar,
+    Stats,
+    Testimonials
+};
