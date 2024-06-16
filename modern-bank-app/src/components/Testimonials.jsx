3d42a81604589cfac91789213e9bbfcd264714da
kshitizvardhan <kshitizvardhan8@gmail.com>
2024-06-17 00:52:49 +0530

    update: testimonials component

--- modern-bank-app/src/components/Testimonials.jsx
+++ modern-bank-app/src/components/Testimonials.jsx
@@ -1,33 +1,29 @@
-import React from 'react'
-import styles from "../style"
-import {feedback} from '../constants'
-import FeedbackCard from './FeedBackCard'
+import { feedback } from "../constants";
+import styles from "../style";
+import FeedBackCard from "./FeedBackCard";
 
-const Testimonials = () => {
-  return (
-    <section id='clients' className={`${styles.paddingY} ${styles.flexCenter} flex-col relative`}>
-      
-      {/* Gradient on right side */}
-      <div className='absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient'/>
+const Testimonials = () => (
+  <section id="clients" className={`${styles.paddingY} ${styles.flexCenter} flex-col relative `}>
+     {/* Gradient on right side */}
+    <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient bottom-40" />
 
-      <div className='w-full justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]'>
-        <h1 className={styles.heading2}>
-          What people are <br className='sm:block hidden'/>saying about us
-        </h1>
-        <div className='w-full md:mt-0 mt-6'>
-          <p className={`${styles.paragraph} text-left max-w-[450px]`}>
-            Everything you need to accept card payments and grow your business anywhere on the planet.
-          </p>
-        </div>
+    <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
+      <h2 className={styles.heading2}>
+        What People are <br className="sm:block hidden" /> saying about us
+      </h2>
+      <div className="w-full md:mt-0 mt-6">
+        <p className={`${styles.paragraph} text-left max-w-[450px]`}>
+          Everything you need to accept card payments and grow your business
+          anywhere on the planet.
+        </p>
       </div>
-      <div className='flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]'>
-        {feedback.map((feed,index)=>(
-          <FeedbackCard key={feed.id} {...feed}/>
-          // Here the spread operator with the object sends the object CONTENT with it to the compoenent
-        ))}
-      </div>
-    </section>
-  )
-}
+    </div>
 
-export default Testimonials
+    <div className="flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]">
+      {feedback.map((card) => <FeedBackCard key={card.id} {...card} />)}
+      {/* Here the spread operator with the object sends the object CONTENT with it to the component */}
+    </div>
+  </section>
+);
+
+export default Testimonials;
